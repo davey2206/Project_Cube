7e546344bf4ba732d8bb6be9ac989422089b796e
Davopia <davey2206@live.nl>
2023-11-08 10:46:19 +0100

    added luck buff

---
+++ Assets/Abilities/_ExtraAbilities/Luck.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Luck : MonoBehaviour
+{
+    [SerializeField] PlayerStats playerStats;
+    [SerializeField] RarityTypes rarity;
+    public void Ability(int level, Vector3 pos)
+    {
+        switch (rarity)
+        {
+            case RarityTypes.Common:
+                playerStats.Luck = playerStats.Luck + 0.5f;
+                break;
+            case RarityTypes.Uncommon:
+                playerStats.Luck = playerStats.Luck + 1f;
+                break;
+            case RarityTypes.Rare:
+                playerStats.Luck = playerStats.Luck + 2f;
+                break;
+            case RarityTypes.Epic:
+                playerStats.Luck = playerStats.Luck + 5f;
+                break;
+            case RarityTypes.Legendary:
+                playerStats.Luck = playerStats.Luck + 10f;
+                break;
+        }
+    }
+}
