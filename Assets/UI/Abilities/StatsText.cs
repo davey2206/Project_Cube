490d6b8c4c84691d0b96ce6928612c4949427660
Davopia <davey2206@live.nl>
2023-11-08 10:56:24 +0100

    added new stats to players stats text

--- Assets/UI/Abilities/StatsText.cs
+++ Assets/UI/Abilities/StatsText.cs
@@ -13,10 +13,13 @@ public class StatsText : MonoBehaviour
     void Update()
     {
         statsText.text = "Max Health " + playerStats.maxHealth.ToString() + Environment.NewLine +
+            "Base Damage " + (playerStats.BaseAttack * 10).ToString() + Environment.NewLine +
             "Bonus Damage " + playerStats.BonusAttack.ToString() + "%" + Environment.NewLine +
-            "Attack Speed " + playerStats.AttackSpeed.ToString() + Environment.NewLine +
             "<color=#ffff00ff>Bonus Yellow Damage " + playerStats.YellowDamage.ToString() + "%</color>" + Environment.NewLine +
             "<color=#008000ff>Bonus Green Damage " + playerStats.GreenDamage.ToString() + "%</color>" + Environment.NewLine +
-            "<color=#00ffffff>Bonus Blue Damage " + playerStats.BlueDamage.ToString() + "%</color>";
+            "<color=#00ffffff>Bonus Blue Damage " + playerStats.BlueDamage.ToString() + "%</color>" + Environment.NewLine +
+            "Attack Speed " + playerStats.AttackSpeed.ToString() + Environment.NewLine +
+            "Crit Rate " + playerStats.critRate.ToString() + "%" + Environment.NewLine +
+            "Crit Damage " + playerStats.critRate.ToString() + "%" + Environment.NewLine;
     }
 }
