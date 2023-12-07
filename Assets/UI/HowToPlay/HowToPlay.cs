181d56dbbe019d73a4d3b570ace489dd334906dd
davey2206 <davey2206@live.nl>
2023-12-07 22:40:33 +0100

    0.1.2 done

--- Assets/UI/HowToPlay/HowToPlay.cs
+++ Assets/UI/HowToPlay/HowToPlay.cs
@@ -18,21 +18,8 @@ public class HowToPlay : MonoBehaviour
 
     public void Next()
     {
-        if (counter != 2)
-        {
-            tutorials[counter].SetActive(false);
-            counter++;
-            tutorials[counter].SetActive(true);
-        }
-        else
-        {
-            counter = 0;
-            tutorials[0].SetActive(true);
-            tutorials[1].SetActive(false);
-            tutorials[2].SetActive(false);
-            menu.SetActive(true);
-            cube.SetActive(true);
-            Tutorial.SetActive(false);
-        }
+        menu.SetActive(true);
+        cube.SetActive(true);
+        Tutorial.SetActive(false);
     }
 }
